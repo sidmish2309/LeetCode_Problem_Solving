61214a115b6c112497d830855a56e5810fa0e929
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2022-02-28 02:29:00 +0530

    Time: 6 ms (36.04%), Space: 9 MB (18.92%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -5,17 +5,17 @@ public:
         int max1=0, max2=0, max3=0;
         
         int n=colors.size();
-        if(colors[0]!=colors[n-1])
+        if(colors[0]!=colors[n-1])  //starting and ending position
             max3=n-1;
         for(int i=1; i<=n-2; i++)
         {
-            if(colors[0]!=colors[i])
+            if(colors[0]!=colors[i])    // max distance of ith index from starting position
             {
                 if(i>max1)
                max1=i; 
             }
                 
-            if(colors[n-1]!=colors[i])
+            if(colors[n-1]!=colors[i])  // max distance of ith index from ending position
             {
                 if(n-1-i>max2)
                 max2=n-1-i;
