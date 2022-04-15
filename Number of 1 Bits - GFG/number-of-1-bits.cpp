b8eb2fdf9d7caa50964be218c7ecf10e3e2eb44f
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2022-04-15 10:43:01 +0530

    Added solution - LeetHub

--- Number of 1 Bits - GFG/number-of-1-bits.cpp
+++ Number of 1 Bits - GFG/number-of-1-bits.cpp
@@ -6,14 +6,7 @@ using namespace std;
 class Solution {
     public:
     int setBits(int N) {
-       int cnt=0;
-       while(N>0)
-       {
-           if(N&1)
-           cnt++;
-           N>>=1;
-       }
-       return cnt;
+      return __builtin_popcount(N);
     }
 };
 
