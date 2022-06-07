8804c5fd6719ce337f7e1b9ec2c01291702cd4e2
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2022-06-08 01:10:05 +0530

    Time: 3 ms (59.53%), Space: 8.4 MB (24.56%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(auto x:nums)
-            cout<<x<<" ";
-        cout<<endl;
+       int z=count(nums.begin(), nums.end(), 0);
+        int o=count(nums.begin(), nums.end(), 1);
+        int t=nums.size()-z-o;
+        
+        vector<int> v;
+        while(z--)
+            v.push_back(0);
+        while(o--)
+            v.push_back(1);
+        while(t--)
+            v.push_back(2);
+        
+        for(int i=0; i<nums.size(); i++)
+            nums[i]=v[i];
+        
+        for(int i=0; i<nums.size(); i++)
+            cout<<nums[i]<<" ";
+        
     }
 };
