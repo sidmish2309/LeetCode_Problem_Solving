8d6bf257f366fa6f5455bd7a9394610962db0674
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2022-06-27 18:54:13 +0530

    Added solution - LeetHub

---
+++ Change Bits - GFG/change-bits.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> changeBits(int N) {
+        // we have to find the most significant set bit
+        //e.g---> 8(1000)--->3
+        // new no. will be---> 15---> (2^(3+1))-1;
+        int mssb=log2(N);
+        mssb++;
+        int p=pow(2,mssb)-1;
+        return {p-N,p};
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        Solution ob;
+        auto ans = ob.changeBits(N);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
