872355491e27f13b73b5f169b80fe00254d62e9a
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2022-08-21 23:47:14 +0530

    Time: 10 ms (47.04%), Space: 8.6 MB (21.13%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -4,14 +4,8 @@ public:
        vector<int> v;
         for(int i=0; i<=n; i++)
         {
-            int x=i;
-            int p=0;
-            while(x!=0)
-            {
-                x=x&(x-1);
-                p++;
-            }
-            v.push_back(p);
+           
+            v.push_back(__builtin_popcount(i));
         }
         
         return v;
