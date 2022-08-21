e45aaa9dca1aaaa8c1c65d25ebf22644ce9104a2
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2022-08-22 02:09:50 +0530

    Time: 5 ms (85.77%), Space: 13.8 MB (26.05%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,6 +4,8 @@ public:
         int n = cost.size();
        
         vector<int> dp(n + 5);
+        dp[0]=0;
+        dp[1]=0;
         
         for (int i = 2; i <= n; i++) {
             int jumpOneStep = dp[i - 1] + cost[i - 1];  
