dea5774c7d2bc17841c2b796a742b105c4bccfee
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2022-10-30 21:31:17 +0530

    Time: 13 ms (36.93%), Space: 11.6 MB (83.84%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -2,20 +2,8 @@ class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
-        vector<int> v;
-        int lower=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        int upper=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-        
-        upper--;
-        cout<<lower<<" "<<upper<<endl;
-        if(lower>upper)
-            return v;
-        if(nums[lower]==target)
-        {
-            for(int i=lower; i<=upper; i++)
-                v.push_back(i);
-        }
-        return v;
-        
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++) if(nums[i]==target) ans.push_back(i);
+        return ans;
     }
 };
