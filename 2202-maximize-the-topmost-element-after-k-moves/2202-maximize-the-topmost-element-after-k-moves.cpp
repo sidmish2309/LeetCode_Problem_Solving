d670eea0424f73afca2f06fc455f202c96cc1afd
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2022-11-30 01:19:29 +0530

    Time: 403 ms (13.07%), Space: 63.5 MB (39.59%) - LeetHub

---
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumTop(vector<int>& nums, int k) {
+        int n=nums.size();
+        int maxi=*max_element(nums.begin(),nums.end());
+        int p=0;
+       
+        if(k&1 && n==1) return -1;
+        if(k==1) return nums[1];
+        if(k>n) return maxi;
+         for(int i=0; i<k-1; i++) p=max(p,nums[i]);
+        if(k==n) return *max_element(nums.begin(),nums.begin()+k-1);
+       
+        return max(p,nums[k]);
+        
+    }
+};
