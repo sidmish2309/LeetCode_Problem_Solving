6b9cd85409d3ec0f1400017342fd2e0eacc4cf6c
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2022-12-03 12:42:20 +0530

    Time: 7 ms (44.98%), Space: 8.2 MB (61.11%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        vector<int> v;
+        int c=0;
+        for(int i=0; i<n-1; i++)
+        {
+            if(arr[i]==arr[i+1]) c++;
+            else
+            {
+                v.push_back(c);
+                c=0;
+            }
+        }
+        
+        v.push_back(c);
+        sort(v.begin(),v.end());
+        for(int i=0; i<v.size()-1; i++)
+        {
+            if(v[i]==v[i+1]) return false;
+        }
+        return true;
+    }
+};
