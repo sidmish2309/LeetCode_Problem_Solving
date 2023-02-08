40ff4519139f9b135208ed3415bb24f5107a5acf
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2023-02-08 21:33:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (64.15%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,20 @@
+#define ll long long
+class Solution {
+public:
+  
+    int reverse(int x) {
+        bool f=false;
+        int g=x;
+        
+        if(x<0) f=true;
+        x=abs(x);
+        string p=to_string(x);
+        std::reverse(p.begin(),p.end());
+        long long ans=stoll(p);
+        
+        if(f) ans*=-1;
+        cout<<ans<<endl;
+         if(ans<INT_MIN || ans>INT_MAX) return 0;
+        return ans;
+    }
+};
