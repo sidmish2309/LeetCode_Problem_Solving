e0c829c37f497c231a0e6a6b185790f9598a2e3a
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2023-06-23 15:08:26 +0530

    Time: 7 ms (74.18%), Space: 13.2 MB (33.83%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,12 +1,10 @@
-#define vi vector<int>
-#define vvi vector < vi >
 class Solution {
 public:
     int dp[30001][2];
     int solve(int ind, int buy, vector<int> & prices)
     {
-        if(ind==prices.size()) return 0;
-        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        
+       
         int profit=0;
         if(buy) profit=max(-prices[ind]+solve(ind+1,0, prices), 0+ solve(ind+1,1,prices));
         
@@ -15,9 +13,22 @@ public:
         return dp[ind][buy]=profit;
     }
     int maxProfit(vector<int>& prices) {
+        int n=prices.size();
       // any no. of transactions
         // dp[i][j]---> max profit from index i to end if I either buy or sell
-        memset(dp,-1,sizeof(dp));
-        return solve(0,1,prices);
+        memset(dp,0,sizeof(dp));
+        for(int ind=n-1; ind>=0; ind--)
+        {
+            for(int buy=0; buy<=1; buy++)
+            {
+           int profit=0;
+        if(buy) profit=max(-prices[ind]+dp[ind+1][0], 0+ dp[ind+1][1]);
+                
+        else profit=max(prices[ind]+dp[ind+1][1], 0+ dp[ind+1][0]);
+        dp[ind][buy]=profit;
+            }
+        }
+        
+       return dp[0][1];
     }
 };
