db76710a52f3b3685eb41e544e1b14f3e49ece02
Siddharth Mishra <71016242+sidmish2309@users.noreply.github.com>
2023-08-26 11:35:35 +0530

    Added solution - LeetHub

--- Longest K unique characters substring - GFG/longest-k-unique-characters-substring.cpp
+++ Longest K unique characters substring - GFG/longest-k-unique-characters-substring.cpp
@@ -9,23 +9,25 @@ using namespace std;
 
 class Solution{
   public:
-
-    int longestKSubstr(string nums, int k) {
-         int longest=-1, i=0, j=0;
-         map<char,int> m;
-         while(j<nums.length())
-         {
-             m[nums[j]]++;
-             while(m.size()>k)
-             {
-                 m[nums[i]]--;
-                 if(m[nums[i]]==0) m.erase(nums[i]);
-                 i++;
-             }
-             if(m.size()==k) longest=max(longest,j-i+1);
-             ++j;
-         }
-         return longest;
+    int longestKSubstr(string s, int k) {
+    map<char,int> m;
+    int i=0, j=0, n=s.length(), ans=-1;
+    while(j<n)
+    {
+        m[s[j]]++;
+        while(m.size()>k)
+        {
+            m[s[i]]--;
+            if(m[s[i]]==0) m.erase(s[i]);
+            i++;
+        }
+        
+        if(m.size()==k) ans=max(ans,j-i+1);
+        
+        j++;
+    }
+    
+    return ans;
     }
 };
 
